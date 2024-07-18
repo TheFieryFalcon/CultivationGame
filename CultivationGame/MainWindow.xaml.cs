293fc58748583ff296bc45af7c197d5c042d38cf
TheFieryFalcon <72846259+TheFieryFalcon@users.noreply.github.com>
2024-07-18 21:34:40 +0800

    added actual stuff + learned xml

--- CultivationGame/MainWindow.xaml.cs
+++ CultivationGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,71 @@ using System.Windows.Shapes;
 
 namespace CultivationGame
 {
+    public class MultiplyConverter : IMultiValueConverter //literally just stolen from stackoverflow, god I fucking hate XAML
+    {
+        public object Convert(object[] values, Type targetType,
+               object parameter, CultureInfo culture)
+        {
+            double result = 1.0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] is double)
+                    result *= (double)values[i];
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes,
+               object parameter, CultureInfo culture)
+        {
+            throw new Exception("Not implemented");
+        }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ImageBrush brush = new();
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Cultivation_Click(object sender, EventArgs e)
+        {
+            
+            if (Game.StopCultSignal == true)
+            {
+                brush.ImageSource = new BitmapImage(new Uri(@"assets\icons\cultivation_button_c.png", UriKind.Relative));
+                Game.StopCultSignal = false;
+                Game.StartCultivating();
+                Cultivate.Background = brush;
+                
+
+            }
+            else
+            {
+                brush.ImageSource = new BitmapImage(new Uri(@"assets\icons\cultivation_button.png", UriKind.Relative));
+                Game.StopCultSignal = true;
+                Game.IsCultivating = false;
+                Cultivate.Background = brush;
+            }
+
+
+
+        }
+
+        private void Breakthrough_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async Task UpdateGame()
+        {
+
         }
     }
 }
